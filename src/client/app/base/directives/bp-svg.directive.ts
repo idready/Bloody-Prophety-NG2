5b58d0e9031e2e0636abc3fc849efbf0e31308d3
idready <you_sombo@hotmail.fr>
2017-05-02 15:27:11 +0200

    Added some options on svg directives too

--- src/client/app/base/directives/bp-svg.directive.ts
+++ src/client/app/base/directives/bp-svg.directive.ts
@@ -5,6 +5,17 @@ import { ElementRef, Directive, Input, OnInit } from '@angular/core';
 })
 export class BpSvgDirective implements OnInit {
     /**
+     * [Input Svg isDecorative, checks if we should flag the svg as decorative by adding an attribute aria-hidden]
+     * @param  boolean 'svgTitle' [Used an alias just as a test]
+     * Refer to https://css-tricks.com/accessible-svgs/ for more infos
+     */
+    @Input('svgIsDecorative') isDecorative: boolean = false;
+    /**
+     * [Input Svg title, here we are dealing with the svg fragment (id)]
+     * @param  string 'svgTitle' [Used an alias just as a test]
+     */
+    @Input('svgTitle') title: string = '';
+    /**
      * [Input Svg name, here we are dealing with the svg fragment (id)]
      * @param  string 'svgName' [Used an alias just as a test]
      */
@@ -28,7 +39,8 @@ export class BpSvgDirective implements OnInit {
         
         let svgFileName: string = (this.file && this.file !== 'svg-defs') ? this.file : 'svg-defs';
         
-        this.element.nativeElement.innerHTML = `<svg class="svg icon-${this.name}" role="img">
+        this.element.nativeElement.innerHTML = `<svg ${this.isDecorative ? 'aria-hidden="true"' : ''} class="icon icon-${this.name}" role="img">
+            <title>${this.title}</title>
             <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="assets/svg-def/${svgFileName}.svg#shapes-icon-${this.name}"></use>
         </svg>`;
     }
